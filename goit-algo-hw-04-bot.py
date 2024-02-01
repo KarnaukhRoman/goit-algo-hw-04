3a94beda0114a59b756e7dcc5bcca8cfde42d179
KarnaukhRoman <karnaukhrv@ukr.net>
2024-02-01 09:45:16 +0100

    Begin create bot

---
+++ goit-algo-hw-04-bot.py
@@ -0,0 +1,26 @@
+
+
+
+def parse_input():
+    pass
+
+def add_contact():
+    pass
+
+def change_contact():
+    pass
+
+def show_phone():
+    pass
+
+def show_all():
+    pass
+
+def main():
+    print("Welcome to the assistant bot!")
+    
+    pass
+
+
+if __name__=="__main__":
+    main()
